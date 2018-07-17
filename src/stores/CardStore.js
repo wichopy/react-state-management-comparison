c78c7a707922042fe49a2c060b52d09befe370ef
William Chou <w.chou06@gmail.com>
2018-07-16 22:33:53 -0400

    Refactor to using mobx

---
+++ src/stores/CardStore.js
@@ -0,0 +1,69 @@
+import { observable, action, decorate } from 'mobx'
+
+class CardStore {
+  cards = [
+    {
+      Assignee: "Winnie",
+      description: "Buy eggs"
+    },
+    {
+      Assignee: "Winnie",
+      description: "Return books"
+    },
+    {
+      Assignee: "Bob",
+      description: "Clean basement"
+    },
+    {
+      Assignee: "Bob",
+      description: "Bring dog to the vet"
+    },
+    {
+      Assignee: "Thomas",
+      description: "Pack for Hawaii"
+    },
+    {
+      Assignee: "Thomas",
+      description: "Study for exam"
+    },
+    {
+      Assignee: "George",
+      description: "Pay electricity bill"
+    },
+    {
+      Assignee: "George",
+      description: "Walk the dog."
+    }
+  ]
+  assignees = ["Winnie", "Bob", "Thomas", "George"]
+
+  addCard = card => this.cards.push(card)
+
+  onShiftLeft = card => {
+    let currentAssigneeIndex = this.assignees.indexOf(card.Assignee)
+    let nextAssignee = this.assignees[currentAssigneeIndex - 1]
+    let cardIndex = this.cards.findIndex(item => item.Assignee === card.Assignee && item.description === card.description)
+    const removedCard = this.cards.splice(cardIndex, 1)[0] //mutates
+    removedCard.Assignee = nextAssignee
+    this.cards.push(removedCard)
+  }
+
+  onShiftRight = card => {
+    let currentAssigneeIndex = this.assignees.indexOf(card.Assignee)
+    let nextAssignee = this.assignees[currentAssigneeIndex + 1]
+    let cardIndex = this.cards.findIndex(item => item.Assignee === card.Assignee && item.description === card.description)
+    const removedCard = this.cards.splice(cardIndex, 1)[0] //mutates
+    removedCard.Assignee = nextAssignee
+    this.cards.push(removedCard)
+  }
+}
+
+decorate(CardStore, {
+  cards: observable,
+  assignees: observable,
+  addCard: action,
+  onShiftLeft: action,
+  onShiftRight: action,
+})
+
+export default CardStore

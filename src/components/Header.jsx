aa6a0020612dbc27282d4ebcd81557204c95b5a9
William Chou <w.chou06@gmail.com>
2018-07-16 22:43:34 -0400

    Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  text-transform: uppercase;
+`;
+
+const Header = ({}) => <H1>Task Board</H1>;
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import { inject, observer } from 'mobx-react'
+import { inject, observer } from "mobx-react";
 
 import Columns from "../components/Columns";
 import Column from "./Column";
-
+import Header from "../components/Header";
 class App extends Component {
-
   render() {
     return (
       <div className="App">
+        <Header />
         <Columns>
           {this.props.CardStore.assignees.map((assignee, i) => {
-              return (
-                <Column
-                  key={assignee + i}
-                  columnName={assignee}
-                  start={i === 0}
-                  end={i === this.props.CardStore.assignees.length - 1}
-                  cards={this.props.CardStore.cards.filter(
-                    card => card.Assignee === assignee
-                  )}
-                />
-              );
-            })}
+            return (
+              <Column
+                key={assignee + i}
+                columnName={assignee}
+                start={i === 0}
+                end={i === this.props.CardStore.assignees.length - 1}
+                cards={this.props.CardStore.cards.filter(
+                  card => card.Assignee === assignee
+                )}
+              />
+            );
+          })}
         </Columns>
       </div>
     );
   }
 }
 
-export default inject('CardStore')(observer(App));
+export default inject("CardStore")(observer(App));

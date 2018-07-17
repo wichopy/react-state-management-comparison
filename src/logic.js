4af967abf17d80f40ce2eed61d0ddb37e16f66c2
William Chou <w.chou06@gmail.com>
2018-07-16 21:53:26 -0400

    Lint error

--- src/logic.js
+++ src/logic.js
@@ -2,7 +2,7 @@ export const onShiftLeft = (card, cards, assignees) => {
   const nextCards = [...cards]
   let currentAssigneeIndex = assignees.indexOf(card.Assignee)
   let nextAssignee = assignees[currentAssigneeIndex - 1]
-  let cardIndex = cards.findIndex(item => item.Assignee == card.Assignee && item.description === card.description)
+  let cardIndex = cards.findIndex(item => item.Assignee === card.Assignee && item.description === card.description)
   const removedCard = nextCards.splice(cardIndex, 1)[0] //immute
   removedCard.Assignee = nextAssignee
   return nextCards.concat(removedCard)
@@ -13,7 +13,7 @@ export const onShiftRight = (card, cards, assignees) => {
   const nextCards = [...cards]
   let currentAssigneeIndex = assignees.indexOf(card.Assignee)
   let nextAssignee = assignees[currentAssigneeIndex + 1]
-  let cardIndex = cards.findIndex(item => item.Assignee == card.Assignee && item.description === card.description)
+  let cardIndex = cards.findIndex(item => item.Assignee === card.Assignee && item.description === card.description)
   const removedCard = nextCards.splice(cardIndex, 1)[0] //immute
   removedCard.Assignee = nextAssignee
   return nextCards.concat(removedCard)
